060611eb1e8e0803bba3756120cb0f7bee1bd667
victordanielb <victordaniel_147@hotmail.com>
2021-11-22 23:57:20 -0300

    ajuste no host

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
+  host: 'ec2-3-142-150-245.us-east-2.compute.amazonaws.com',
   username: 'docker',
   password: 'docker',
   database: 'sqlnode',
